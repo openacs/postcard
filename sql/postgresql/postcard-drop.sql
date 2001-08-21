f5199464c9b6c3a1b64df3f606bbde50dc39b3df
Kapil Thangavelu <k_vertigo@yahoo.com>
2001-08-21 22:50:55 +0000

    removed trigger delete

--- sql/postgresql/postcard-drop.sql
+++ sql/postgresql/postcard-drop.sql
@@ -2,7 +2,7 @@
 
 
 drop table postcard_images;
-drop trigger postcard_images_lob_trigger;
+--drop trigger postcard_images_lob_trigger;
 drop table postcards;
 drop sequence postcard_seq;
 drop sequence postcard_image_seq;
