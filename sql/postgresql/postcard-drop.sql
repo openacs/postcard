5af0e275b61e31c548d7068aeab5a00f6fe4ba8b
Kapil Thangavelu <k_vertigo@yahoo.com>
2001-08-21 22:15:43 +0000

    added basic permissioning and pg lob support

--- sql/postgresql/postcard-drop.sql
+++ sql/postgresql/postcard-drop.sql
@@ -1,5 +1,34 @@
+
+
+
 drop table postcard_images;
+drop trigger postcard_images_lob_trigger;
 drop table postcards;
 drop sequence postcard_seq;
 drop sequence postcard_image_seq;
+
+
+-- Drop permission metadata
+delete from acs_permissions
+    where privilege in
+        ('postcard_create_card', 'postcard_create_image',
+        'postcard_read', 'postcard_admin');
+
+
+delete from acs_privilege_hierarchy
+    where privilege in
+        ('postcard_create_card', 'postcard_create_image',
+        'postcard_read', 'postcard_admin');
+
+delete from acs_privilege_hierarchy
+    where child_privilege in
+        ('postcard_create_card', 'postcard_create_image',
+        'postcard_read', 'postcard_admin');
+
+delete from acs_privileges
+    where privilege in
+        ('postcard_create_card', 'postcard_create_image',
+        'postcard_read', 'postcard_admin');
+
+
 
